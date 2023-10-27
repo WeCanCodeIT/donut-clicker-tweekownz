bf65f8ee76db47ff5f6f3f877208273fe8c69091
John Doe <tweekownz@gmail.com>
2023-10-26 23:07:38 -0400

    Adding buttons

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const countdownTimer = document.getElementById("countdown-timer")
-let timeLeft = 0
-let autoClicker = 10
+const counter = document.getElementById("counter")
+
+let startCount = 0;
+let autoClicker = 1;
+let click = 1;
+
+let powerOne = false;
+let powerTwo = false;
+let powerThree = false;
+let powerFour = false;
+let clickOne = false;
+let clickTwo = false;
+let clickThree = false;
+let clickFour = false;
 
 
-function renderTime(){
-    countdownTimer.innerHTML = timeLeft;
+function autoCount(){
+    counter.innerText = startCount;
 }
 
 setInterval(function (){
-    renderTime()
-    timeLeft = timeLeft + autoClicker
+    autoCount()
+    startCount = startCount + autoClicker
 }, 1000)
+
+// if (x > 50) {
+//     /* do something */
+//   } else if (x > 5) {
+//     /* do something */
+//   } else {
+//     /* do something */
+//   }
 
 
